e5851adf5fdea26101fcd0eb4c1d6d8530e8ab8e
Justin Seliga <justinsel@zillowgroup.com>
2026-03-23 00:32:55 -0400

    wip

--- tests/Fixtures/Tooling/Http/Api/PhpStan/Schema.php
+++ tests/Fixtures/Tooling/Http/Api/PhpStan/Schema.php
@@ -3,10 +3,10 @@
 namespace Tests\Fixtures\Tooling\Http\Api\PhpStan;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Support\Http\Resources\Schemas\Contracts\Schema as SchemaContract;
+use Support\Http\Resources\Schemas\Contracts;
 use Support\Http\Resources\Schemas\Provides\AsSchema;
 
-final class Schema extends JsonResource implements SchemaContract
+final class Schema extends JsonResource implements Contracts\Schema
 {
     use AsSchema;
 
