c8fb6751ef3f89273c8251233494e03207affb03
Andrew Weber <andrewweb@showingtimeplus.com>
2026-04-02 12:13:48 -0500

    code style

--- tests/Fixtures/Support/Http/Api/Resources/Json/PaginatedResourceResponse/StringableSort.php
+++ tests/Fixtures/Support/Http/Api/Resources/Json/PaginatedResourceResponse/StringableSort.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace Tests\Fixtures\Support\Http\Api\Resources\Json\PaginatedResourceResponse;
 
 use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 final class StringableSort implements Castable
 {
     public function __construct(
         public string $field
-    ) {
-    }
+    ) {}
 
+    /**
+     * @param string[] $arguments
+     * @return class-string<CastsAttributes<StringableSort, StringableSort|mixed>>
+     */
     public static function castUsing(array $arguments): string
     {
         return StringableSortCast::class;
